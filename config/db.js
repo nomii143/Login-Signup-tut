43d85c210acd8ebe506987c8cf5beb7ae1d48a03
Noman Khan <nomanswat0@gmail.com>
2025-07-13 13:13:32 +0500

    Fix User model import and login error

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
+require('dotenv').config(); // Load environment variables
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Login-Signup-tut');
-    console.log('MongoDB connected to Login-Signup-tut');
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected to ${mongoose.connection.name}`);
   } catch (err) {
     console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit process with failure
   }
 };
 
--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = require('./api/app');
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
 });
